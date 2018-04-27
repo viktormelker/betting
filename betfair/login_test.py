aa21b496353c638e3cd1c70b69e030083250c3d2
Viktor Melker <viktormelker@gmail.com>
2018-04-27 12:42:41 +0200

    Yapf

--- betfair/login_test.py
+++ betfair/login_test.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-
 """
 Python 2
 """
 
-
 import requests
-
 
 ### Fill these out! ###
 username = ''
@@ -15,9 +12,16 @@ application_key = ''
 #######################
 
 payload = 'username={}&password={}'.format(username, password)
-headers = {'X-Application': application_key, 'Content-Type': 'application/x-www-form-urlencoded'}
+headers = {
+    'X-Application': application_key,
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
-response = requests.post('https://identitysso.betfair.com/api/certlogin', data=payload, cert=('certs/client-2048.crt', 'certs/client-2048.key'), headers=headers)
+response = requests.post(
+    'https://identitysso.betfair.com/api/certlogin',
+    data=payload,
+    cert=('certs/client-2048.crt', 'certs/client-2048.key'),
+    headers=headers)
 
 if response.status_code == 200:
     response_json = response.json()
