c85a9a5f1d0eb699f6101895d1b22419dc29c1b2
Viktor Melker <viktormelker@gmail.com>
2018-04-27 12:49:23 +0200

    Cleaned up

--- betfair/login_test_library.py
+++ betfair/login_test_library.py
@@ -1,31 +1,24 @@
+"""
+python 3
+"""
+
 from betfair import Betfair
 from betfair.models import MarketFilter, EventTypeResult
 from betfair import utils
 
-client = Betfair('5Tkp9UblKTjP47QC', 'certs/client-2048.pem')
 
-client.login('vime0595', 'b02460246')
+### Fill these out! ###
+username = ''
+password = ''
+application_key = ''
+#######################
+
+client = Betfair(application_key, 'certs/client-2048.pem')
+
 try:
-    import ipdb; ipdb.set_trace()
+    client.login(username, password)
 
-    mf = MarketFilter(text_query='tennis')
-    locale = utils.get_kwargs(locals())
-
-    test_params={}
-
-    filter = mf
-    event_types = client.make_api_request(
-                base='Sports',
-                method='listEventTypes',
-                params=test_params,
-                model=EventTypeResult,
-            )
-
-    #event_types = client.list_event_types(mf)
-
-    print(len(event_types))                 # 2
-    print(event_types[0].event_type.name)   # 'Tennis'
-
+    print(client.session_token)
 
 finally:
     client.logout()
