0d060d6b0099bc3fe4f249c4232192b753540803
Viktor Melker <viktormelker@gmail.com>
2018-04-27 12:38:59 +0200

    Add easily editable credentials

--- betfair/login_test.py
+++ betfair/login_test.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+"""
+Python 2
+"""
+
+
 import requests
 
-payload = 'username=myusername&password=password'
-headers = {'X-Application': 'SomeKey', 'Content-Type': 'application/x-www-form-urlencoded'}
 
-resp = requests.post('https://identitysso.betfair.com/api/certlogin', data=payload, cert=('certs/client-2048.crt', 'certs/client-2048.key'), headers=headers)
+### Fill these out! ###
+username = ''
+password = ''
+application_key = ''
+#######################
 
-if resp.status_code == 200:
-    resp_json = resp.json()
-    print resp_json['loginStatus']
-    print resp_json['sessionToken']
+payload = 'username={}&password={}'.format(username, password)
+headers = {'X-Application': application_key, 'Content-Type': 'application/x-www-form-urlencoded'}
+
+response = requests.post('https://identitysso.betfair.com/api/certlogin', data=payload, cert=('certs/client-2048.crt', 'certs/client-2048.key'), headers=headers)
+
+if response.status_code == 200:
+    response_json = response.json()
+    print response_json['loginStatus']
+    print response_json['sessionToken']
 else:
     print "Request failed."
